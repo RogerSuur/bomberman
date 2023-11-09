08201bd3c3156dd6911de4c4710a9f334af81d85
inquisitivetable <88971685+inquisitivetable@users.noreply.github.com>
2023-11-09 15:42:02 +0200

    timer fixed

--- backend/websocket/websocket.js
+++ backend/websocket/websocket.js
@@ -1,3 +1,5 @@
+import Timer from "../timer.js";
+
 const GetUserlist = (sockets) => {
   let userlist = [];
   for (const socket of sockets) {
@@ -8,12 +10,38 @@ const GetUserlist = (sockets) => {
 
 const MAX_CONNECTIONS = 4;
 
+let timeoutId;
+
+const menuCountdown = (io) => {
+  io.emit("menu countdown");
+  const menuCountdownTimer = setTimeout(() => {
+    gameCountdown(io);
+  }, 20000);
+  timeoutId = menuCountdownTimer;
+};
+
+const gameCountdown = (io) => {
+  io.emit("game countdown");
+  const gameStartTimer = setTimeout(() => {
+    GameStart(io);
+  }, 10000);
+  clearTimeout(timeoutId);
+  timeoutId = gameStartTimer;
+};
+
+const GameStart = (io) => {
+  io.emit("start");
+};
+
+const connectionsCount = (io, conns) =>
+  conns === 4 ? gameCountdown(io) : conns === 2 && menuCountdown(io);
+
 const Websocket = (io) => {
   io.on("connection", async (socket) => {
     const connections = await io.fetchSockets();
 
     if (connections.length <= MAX_CONNECTIONS) {
-      console.log("A user connected here");
+      connectionsCount(io, connections.length);
 
       socket.on("chat message", (msg) => {
         io.emit("chat message", msg);
@@ -21,8 +49,7 @@ const Websocket = (io) => {
 
       socket.on("username", async (username) => {
         socket.data.username = username;
-        console.log(socket.data.username);
-        io.emit("user joined", socket.data.username);
+        socket.broadcast.emit("user joined", socket.data.username);
       });
 
       socket.on("stateUpdate", () => {
@@ -31,15 +58,19 @@ const Websocket = (io) => {
 
       socket.on("disconnecting", () => {
         console.log(`A user ${socket.data.username} disconnected`);
-        io.emit("user left", socket.data.username);
+        socket.broadcast.emit("user left", socket.data.username);
       });
 
       socket.on("disconnect", () => {
+        connections.length < 3 &&
+          timeoutId &&
+          socket.emit("countdown stopped") &&
+          clearTimeout(timeoutId);
         console.log("A user disconnected");
       });
     } else {
       console.log("Connection denied: Maximum clients reached");
-      socket.emit("game full");
+      io.emit("game full");
       socket.disconnect(true);
     }
   });
