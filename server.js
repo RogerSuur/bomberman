d202c72ae48c7c7e39583a41b1ff11eebf26de39
RogerSuur <roger.suuressaar@gmail.com>
2024-12-02 19:24:55 +0200

    more error logs

--- server.js
+++ server.js
@@ -66,3 +66,13 @@ process.on('SIGINT', () => {
         process.exit(0);
     });
 });
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    process.exit(1);
+});
